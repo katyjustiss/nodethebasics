456cc3cee796a5eeb004232ca49ff8ed67f8f693
Katy Justiss <katy.justiss@gmail.com>
2015-07-30 15:27:38 -0500

    added var port to app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 var chalk = require('chalk')
+var port = process.env.PORT || 1337;
 
-require('./lib/server')(1337);
+require('./lib/server')(port);
 
 // var server = require('./lib/server');
-
 // server(1337);
 
-console.log('server running on' + ' ' + chalk.magenta.bold.underline("http://localhost:1337"))
+console.log('server running on' + ' ' + chalk.magenta.bold.underline("http://localhost:" + port))
